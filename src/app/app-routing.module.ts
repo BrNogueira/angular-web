c20f6994db8a86b7d04e7cebff07681be6a70a29
BrNogueira <rodrigo.nfurtado@gmail.com>
2019-01-28 08:50:27 -0200

    [angular]web

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ManterGaleriaComponent } from './componentes/galeria/manter-galeria/manter-galeria.component';
+import { HomeCarrouselComponent } from './componentes/home/home-carrousel/home-carrousel.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home/home-carrousel', component: HomeCarrouselComponent},
+  {path: 'galeria/manter-galeria/', component: ManterGaleriaComponent},
+  {path: 'galeria/manter-galeria/:id', component: ManterGaleriaComponent},
+  {path: '', component: HomeCarrouselComponent},
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeCarrouselComponent } from './componentes/home/home-carrousel/home-carrousel.component';
+import { ManterGaleriaComponent } from './componentes/galeria/manter-galeria/manter-galeria.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeCarrouselComponent,
+    ManterGaleriaComponent
   ],
   imports: [
     BrowserModule,
